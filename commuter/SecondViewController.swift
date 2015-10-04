044a1e147765e43be622f3024f5a8e90915ced0c
Tsukasa Komiyama <apps.by.komi@gmail.com>
2015-10-04 18:24:30 +0900

    Initial Commit

---
+++ commuter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  commuter
+//
+//  Created by 小宮山 司 on 2015/10/04.
+//  Copyright © 2015年 Tsukasa K. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
