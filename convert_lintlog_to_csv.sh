25a06f58e99e1e07168d76e9f77d988b68060f28
Tsukasa Komiyama <apps.by.komi@gmail.com>
2016-02-07 12:45:20 +0900

    fix typo

--- convert_lintlog_to_csv.sh
+++ convert_lintlog_to_csv.sh
@@ -1,2 +1,2 @@
-#!/bin/shq
+#!/bin/sh
 cat ./lint_result.txt | cut -f 2 -d" " | sort | uniq -c | grep -E "\d+\s(error:|warning:)" | xargs | sed -e "s/error: /,/g" | sed -e "s/ warning://" >> count.csv
